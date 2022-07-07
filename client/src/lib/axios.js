c7e869fa73dc44ca1ab0af085560abc8294cdcf6
Kurt <kurt.dacudag@sun-asterisk.com>
2022-07-07 11:41:43 +0800

    Fix

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -4,6 +4,7 @@ const axios = Axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
+    "Content-Type": "application/json",
   },
 });
 
